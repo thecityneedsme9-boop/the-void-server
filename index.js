f9742aeffff65b574a9bc3fe3f9c98ff3bbc9b85
thecityneedsme9-boop <thecityneedsme9@gmail.com>
2026-03-09 21:23:34 +0600

    index.js

--- index.js
+++ index.js
@@ -2,78 +2,62 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const axios = require('axios');
 
 const app = express();
 app.use(cors()); 
 
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: "*" } 
+// 1. THIS FIXES THE 404 FOR GOOD
+app.get('/', (req, res) => {
+    res.status(200).send('The Void Server is ALIVE and routing traffic.');
 });
 
-let activeUsers = 0;
-let vanishedChats = 8400; 
+const server = http.createServer(app);
+
+// 2. THIS ALLOWS YOUR PC HTML FILE TO CONNECT
+const io = new Server(server, {
+    cors: { origin: "*", methods: ["GET", "POST"] } 
+});
+
 let waitingUser = null; 
 
-function getFlagEmoji(countryCode) {
-    if (!countryCode) return '🏳️';
-    const codePoints = countryCode
-        .toUpperCase()
-        .split('')
-        .map(char =>  127397 + char.charCodeAt());
-    return String.fromCodePoint(...codePoints);
-}
-
 io.on('connection', (socket) => {
-    activeUsers++;
-    io.emit('stats_update', { activeUsers, vanishedChats });
-
-    socket.on('find_match', async (userData) => {
-        let userFlag = '🏳️';
-        try {
-            const ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
-            const response = await axios.get(`http://ip-api.com/json/${ip.split(',')[0]}`);
-            if (response.data && response.data.countryCode) {
-                userFlag = getFlagEmoji(response.data.countryCode);
-            }
-        } catch (error) { console.log("IP fetch failed"); }
-
-        socket.userData = { id: socket.id, flag: userFlag, karma: userData.localKarma || "5.0" };
-
+    
+    socket.on('find_match', (userData) => {
         if (waitingUser && waitingUser.id !== socket.id) {
+            // Pair them up
             const roomName = `room_${waitingUser.id}_${socket.id}`;
             socket.join(roomName);
             waitingUser.join(roomName);
-            socket.emit('match_found', { flag: waitingUser.userData.flag, karma: waitingUser.userData.karma });
-            waitingUser.emit('match_found', { flag: socket.userData.flag, karma: socket.userData.karma });
+            
+            io.to(roomName).emit('match_found', { flag: '🏳️', karma: '5.0' });
+            
             socket.room = roomName;
             waitingUser.room = roomName;
             waitingUser = null; 
-        } else { waitingUser = socket; }
+        } else {
+            waitingUser = socket;
+        }
     });
 
     socket.on('send_message', (text) => {
-        if (socket.room) { socket.to(socket.room).emit('receive_message', text); }
+        if (socket.room) {
+            socket.to(socket.room).emit('receive_message', text);
+        }
     });
 
-    socket.on('typing', () => {
-        if (socket.room) { socket.to(socket.room).emit('stranger_typing'); }
-    });
-
-    socket.on('chat_shattered', () => {
-        vanishedChats++;
-        io.emit('stats_update', { activeUsers, vanishedChats });
-    });
-
+    // 3. THIS GUARANTEES THE SHATTER EFFECT TRIGGERS
     socket.on('disconnect', () => {
-        activeUsers--;
-        io.emit('stats_update', { activeUsers, vanishedChats });
-        if (waitingUser && waitingUser.id === socket.id) { waitingUser = null; }
-        if (socket.room) { socket.to(socket.room).emit('stranger_disconnected'); }
+        if (waitingUser && waitingUser.id === socket.id) {
+            waitingUser = null;
+        }
+        if (socket.room) {
+            socket.to(socket.room).emit('stranger_disconnected');
+        }
     });
 });
 
-server.listen(3000, () => {
-    console.log(`The Void Server is alive on port 3000`);
+// 4. THIS BINDS TO RENDER'S EXACT PORT
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`The Void is breathing on port ${PORT}`);
 });
